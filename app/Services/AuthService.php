c9d79fc078d9fce3176563d25ff476f4d2283094
Okan Aslan <aaslan.okan@gmail.com>
2024-06-27 22:12:54 +0300

    created controller, service, repo files for authentication

---
+++ app/Services/AuthService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\AuthRepository;
+
+class AuthService
+{
+    protected $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+}
