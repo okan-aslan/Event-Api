f0311cb27b851d406c2a60539979ea187477add2
Okan Aslan <aaslan.okan@gmail.com>
2024-06-28 16:26:06 +0300

    added Event model relation

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Venue extends Model
 {
@@ -11,4 +12,13 @@ class Venue extends Model
 
     protected $fillable = ['name', 'address', 'country', 'city', 'capacity'];
 
+    /**
+     * Get all of the events for the Venue
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function events(): HasMany
+    {
+        return $this->hasMany(Event::class);
+    }
 }
