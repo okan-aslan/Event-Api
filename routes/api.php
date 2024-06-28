a9ae1041b9e43cab9db4d24b6efa5e479bbacb62
Okan Aslan <aaslan.okan@gmail.com>
2024-06-28 15:24:21 +0300

    added routes for Venue model

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VenueController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
@@ -10,4 +11,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [AuthController::class, 'profile']);
     Route::post('delete', [AuthController::class, 'destroy']);
     Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::get('venues', [VenueController::class, 'index']);
+Route::get('venue/{venue}', [VenueController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('venues/create', [VenueController::class, 'store']);
+    Route::put('venues/{venue}', [VenueController::class, 'update']);
+    Route::delete('venues/{venue}', [VenueController::class, 'destroy']);
 });
