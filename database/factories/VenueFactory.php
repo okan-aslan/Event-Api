61435bbccc88b86cbe547016f8f73c58e075322d
Okan Aslan <aaslan.okan@gmail.com>
2024-06-28 16:15:44 +0300

    added fake data for Venue model

---
+++ database/factories/VenueFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Venue;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Venue>
+ */
+class VenueFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Venue::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'address' => $this->faker->address(),
+            'country' => $this->faker->country(),
+            'city' => $this->faker->city(),
+            'capacity' => $this->faker->numberBetween(500, 5000),
+        ];
+    }
+}
