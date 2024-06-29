dee7b721d52ffad4f87eb9a99a56a4e174df5da3
Okan Aslan <aaslan.okan@gmail.com>
2024-06-29 12:58:34 +0300

    created EventFactory & added fake datas

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'date_time' => $this->faker->dateTimeBetween('now', '+1 year'),
+            'ticket_stock' => $this->faker->numberBetween(50, 5000),
+        ];
+    }
+}
