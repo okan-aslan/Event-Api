5fb7dab4d105ddce8b031736a80d96160938ff20
Okan Aslan <aaslan.okan@gmail.com>
2024-06-29 12:56:43 +0300

    added fake event data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use App\Models\User;
 use App\Models\Venue;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(3)->create();
         Venue::factory(15)->create();
+
+        Event::factory(5)->create([
+            'user_id' => function () {
+                return User::all()->random()->id;
+            },
+            'venue_id' => function () {
+                return Venue::all()->random()->id;
+            },
+        ]);
     }
 }
