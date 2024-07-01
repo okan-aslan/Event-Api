890c33c9576593af88cac7a9e680df2a4ba76d1b
Okan Aslan <aaslan.okan@gmail.com>
2024-07-01 10:15:27 +0300

    extend Base Repository & added local methods

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -4,27 +4,15 @@ namespace App\Repositories;
 
 use App\Models\User;
 
-class AuthRepository
+class AuthRepository extends EloquentModelRepository
 {
-    protected User $model;
-
     public function __construct(User $model)
     {
-        $this->model = $model;
-    }
-
-    public function create(array $data): User
-    {
-        return $this->model->create($data);
+        parent::__construct($model);
     }
 
     public function findByEmail(string $email): User
     {
         return $this->model->where('email', $email)->first();
-    }
-
-    public function destroy(string $id): int
-    {
-        return $this->model->destroy($id);
     }
 }
