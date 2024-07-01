e6f37732343883f45700e5d8d1a2aef9e50e7835
Okan Aslan <aaslan.okan@gmail.com>
2024-07-01 10:15:05 +0300

    extend Base Repository

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -5,41 +5,10 @@ namespace App\Repositories;
 use Illuminate\Database\Eloquent\Collection;
 use App\Models\Event;
 
-class EventRepository
+class EventRepository extends EloquentModelRepository
 {
-    protected Event $model;
-
     public function __construct(Event $model)
     {
-        $this->model = $model;
-    }
-
-    public function getAllEvents(): Collection
-    {
-        return $this->model->all();
-    }
-
-    public function store(array $data): Event
-    {
-        return $this->model->create($data);
-    }
-
-    public function findOrFail(string $id): Event
-    {
-        return $this->model->findOrFail($id);
-    }
-
-    public function update(array $data, string $id): bool
-    {
-        $model = $this->findOrFail($id);
-
-        return $model->update($data);
-    }
-
-    public function destroy(string $id): bool|null
-    {
-        $model = $this->findOrFail($id);
-
-        return $model->delete($id);
+        parent::__construct($model);
     }
 }
--- app/Repositories/VenueRepository.php
+++ app/Repositories/VenueRepository.php
@@ -3,43 +3,11 @@
 namespace App\Repositories;
 
 use App\Models\Venue;
-use Illuminate\Database\Eloquent\Collection;
 
-class VenueRepository
+class VenueRepository extends EloquentModelRepository
 {
-    protected $model;
-
     public function __construct(Venue $model)
     {
-        $this->model = $model;
-    }
-
-    public function getAllVenues(): Collection
-    {
-        return $this->model->all();
-    }
-
-    public function store(array $data): Venue
-    {
-        return $this->model->create($data);
-    }
-
-    public function findOrFail(string $id): Venue
-    {
-        return $this->model->findOrFail($id);
-    }
-
-    public function update(array $data, string $id): bool
-    {
-        $model = $this->findOrFail($id);
-
-        return $model->update($data);
-    }
-
-    public function destroy(string $id)
-    {
-        $model = $this->findOrFail($id);
-
-        return $model->delete($id);
+        parent::__construct($model);
     }
 }
